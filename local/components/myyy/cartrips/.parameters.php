0f36a32a9e84d095b45e391d21f4a6d10187d9a2
AndreyPoly144 <111175855+AndreyPoly144@users.noreply.github.com>
2023-05-28 11:21:01 +0500

    Update .parameters.php

--- local/components/myyy/cartrips/.parameters.php
+++ local/components/myyy/cartrips/.parameters.php
@@ -1,24 +1,3 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
-//if(!CModule::IncludeModule("iblock"))
-//    return;
-
-//получаем все ид инфоблоков для выбраного типа инфоблока
-//$arIBlocks=["-"=>" "];
-//$db_iblock = CIBlock::GetList(["SORT"=>"ASC"], ["SITE_ID"=>$_REQUEST["site"],
-//    "TYPE" => ($arCurrentValues["IBLOCK_TYPE"]!="-"?$arCurrentValues["IBLOCK_TYPE"]:"")]);
-//while($arRes = $db_iblock->Fetch())
-//    $arIBlocks[$arRes["ID"]] = "[".$arRes["ID"]."] ".$arRes["NAME"];
-$iblockType=16;   //тип инфоблока в котором находятся все нужные для задания инфоблоки
-
-$arComponentParameters=[
-    "PARAMETERS" => [
-        "IBLOCK_TYPE" => [
-            "PARENT" => "BASE",
-            "NAME" => GetMessage("TEST_IBLOCK_LIST_TYPE"),
-            "TYPE" => "STRING",
-            "VALUES" => $iblockType,
-        ],
-    ]
-];
